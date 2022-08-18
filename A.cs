017ef78274b524fd5f1a912312a28331945535c0
MaddhuCh <100910796+MaddhuCh@users.noreply.github.com>
2022-08-18 20:44:43 +0100

    Add files via upload

---
+++ A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutilityTest
+{
+    public class A
+    {
+        static void Main(string[] args)
+        {
+            // Print multiples of 4 between 0 and 50 inclusive
+            for (var num = 0; num <= 50; num++)
+            {
+                if (num % 4 == 0)
+                {
+                    Console.WriteLine($"{num} is a multiple of 4.");
+                }
+            }
+            // Print odd numbers 0 and 50 inclusive
+            for (var num = 0; num <= 50; num++)
+            {
+                if (num % 2 == 1)
+                {
+                    Console.WriteLine($"{num} is an odd number.");
+                }
+
+            }
+        }
+    }
+}
---
+++ C.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutilityTest
+{
+    public class C
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the upper limit number:");
+            var i = Convert.ToInt32(Console.ReadLine());
+            // Print multiples of 4 between 0 and 50 inclusive
+            for (var num = 0; num <= i; num++)
+            {
+                if (num % 4 == 0)
+                {
+                    Console.WriteLine($"{num} is a multiple of 4.");
+                }
+            }
+            // Print odd numbers 0 and 50 inclusive
+            for (var num = 0; num <= i; num++)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine($"{num} is an odd number.");
+                }
+
+            }
+
+        }
+    }
+}
